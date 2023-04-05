10261de20bdf4ded7ec6314a035602a755880f63
Abdul Ajees S <108514085+Ajees008@users.noreply.github.com>
2023-04-05 11:07:27 +0530

    Add files via upload

---
+++ src/components/Home/Header/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './styles.css';
+
+const Header = () => (
+  <header className='home-header'>
+  
+    <h1>
+    <span>“</span>Ajees<span>”</span> <br/>Blog App 
+    </h1>
+    <p>
+      Awesome place to make oneself <br /> productive and entertained through
+      daily updates.
+    </p>
+  </header>
+);
+
+export default Header;
---
+++ src/config/data.js
@@ -0,0 +1,87 @@
+export const blogList = [
+  {
+    id: 1,
+    title: '"What we think, we become."',
+    category: 'development',
+    subCategory: ['frontend', 'ui/ux', 'design'],
+    description:
+      "In the economics study of the public sector, economic and social development is the process by which the economic well-being and quality of life of a nation, region, local community, or an individual are improved according to targeted goals and objectives.",
+    authorName: 'Abdul Ajees',
+    authorAvatar: '/assets/images/author.jpg',
+    createdAt: 'June 03, 2021',
+    cover: '/assets/images/designer-1.jpg',
+  },
+  {
+    id: 2,
+    title: 'Milan Places That Highlight The City',
+    category: 'travel',
+    subCategory: ['vacation', 'holidays', 'sight seeing'],
+    description:
+      "Milan is known for the majestic Duomo Cathedral, the prominent opera La Scala, the medieval Sforza Castle, the dazzling Vittorio Emanuele II shopping arcade, and Leonardo Da Vinci's legacy. Milan is also famous for its art, history, architecture, and football, as well as extraordinary fashion and culinary scenes.",
+    authorName: 'Abdul Ajees',
+    authorAvatar: '/assets/images/author.jpg',
+    createdAt: 'June 04, 2022',
+    cover: '/assets/images/f67396fc3cfce63a28e07ebb35d974ac.jpg',
+  },
+  {
+    id: 3,
+    title: 'Online Shopping – An Alternative to Shopping in the Mall',
+    category: 'shopping',
+    subCategory: ['e-commerce store', 'clothing', 'shopping store'],
+    description:
+      "Online shopping is a form of electronic commerce which allows consumers to directly buy goods or services from a seller over the Internet using a web browser or a mobile app. Consumers find a product of interest by visiting the website of the retailer directly or by searching among alternative vendors using a shopping search engine, which displays the same product's availability and pricing at different e-retailers. As of 2020, customers can shop online using a range of different computers and devices, including desktop computers, laptops, tablet computers and smartphones.",
+    authorName: 'Abdul Ajees',
+    authorAvatar: '/assets/images/author.jpg',
+    createdAt: 'April 03, 2022',
+    cover: '/assets/images/fQwuyKJ9qxjSbr6REcgtmW-1200-80.jpg',
+  },
+  {
+    id: 4,
+    title: 'ADVENTURE IN YOU',
+    category: 'adventure',
+    subCategory: ['adrenaline', 'stay-fit', 'lifestyle'],
+    description:
+      "Adventure is defined as an unusual and exciting, typically hazardous, experience or activity. This is exactly what many biblical figures underwent as they were called upon to go into the unknown in the work of God. Our life can be an adventure in the service of righteousness, facing the adversity and evils of the world. Find inspiration to live adventurously from this collection of Bible verses..",
+    authorName: 'Abdul Ajees',
+    authorAvatar: '/assets/images/author.jpg',
+    createdAt: 'June 09, 2022',
+    cover: '/assets/images/graphic-design-trends.png',
+  },
+  {
+    id: 5,
+    title: 'Loaded BBQ Baked Potatoes',
+    category: 'cooking',
+    subCategory: ['bbq', 'food', 'lifestyle'],
+    description:
+      "It's a good idea to boil potatoes in salted water for about five minutes before direct grilling them. The salt in the water will help flavor the potatoes. Also, boiling will partially cook the potatoes which will let you direct grill at a higher temperature, for a crispier exterior.",
+    authorName: 'Abdul Ajees',
+    authorAvatar: '/assets/images/author.jpg',
+    createdAt: 'May 03, 2022',
+    cover: '/assets/images/make-it-personal.jpg',
+  },
+  {
+    id: 6,
+    title: 'Beyond the Beach',
+    category: 'travel',
+    subCategory: ['beaches', 'sea', 'holidays'],
+    description:
+      "A beach is a landform alongside a body of water which consists of loose particles. The particles composing a beach are typically made from rock, such as sand, gravel, shingle, pebbles, etc., or biological sources, such as mollusc shells or coralline algae.",
+    authorName: 'Abdul Ajees',
+    authorAvatar: '/assets/images/author.jpg',
+    createdAt: 'May 03, 2022',
+    cover:
+      '/assets/images/Purple-Combination-colors-graphic-design-predictions-1024x576-1024x576.jpg',
+  },
+  {
+    id: 7,
+    title: 'Art & Perception',
+    category: 'art',
+    subCategory: ['skill', 'design', 'passion'],
+    description:
+      "There exist various psychological theories, each explaining in its own way the processes of artistic creativity or perception. Very few of them, however, have ever been pursued to an end. There is no completely and generally accepted system of art psychology. Some authors, such as Müller-Freienfels, who want to consolidate all that has been done in this field, give only an eclectic report or synopsis of disparate viewpoints. Most psychologists worked only on some particular problems of the general theory of art and proceeded in their research from different approaches, following divergent paths, and reaching different conclusions. Without a general idea or a valid methodological principle it is difficult to appraise systematically what psychology has achieved in this direction..",
+    authorName: 'Abdul Ajees',
+    authorAvatar: '/assets/images/author.jpg',
+    createdAt: 'June 03, 2020',
+    cover: '/assets/images/Synthwave-Postmodern.jpg',
+  },
+];
